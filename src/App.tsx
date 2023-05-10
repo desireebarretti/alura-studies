00431736d89608503bb358cd7cea1a19bd86f449
desireebarretti <110750885+desireebarretti@users.noreply.github.com>
2023-05-09 21:41:16 -0300

    Criando a lista de estudos

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Botao from './componentes/Botao/Botao';
 import Formulario from './componentes/Formulario/Formulario';
+import Lista from './componentes/Lista/Lista';
 
 
 function App() {
   return (
     <div className="App">
       <Formulario />
+      <Lista />
     </div>
   );
 }
